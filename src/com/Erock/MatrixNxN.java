64be4e66c60d61816618486727dc5c886f7b7051
Gleb Bezborodov <bezborodoff.gleb@gmail.com>
2022-03-13 19:06:48 +0300

    minimal interface

--- src/com/Erock/MatrixNxN.java
+++ src/com/Erock/MatrixNxN.java
@@ -6,4 +6,22 @@ package com.Erock;
 
 public class MatrixNxN {
 
+    private double[] matrixImpl;
+
+    public MatrixNxN() {
+
+    }
+
+    public double det() {
+        return 0.0;
+    }
+
+    public MatrixNxN inv() {
+        return new MatrixNxN();
+    }
+
+    public MatrixNxN transpose() {
+        return new MatrixNxN();
+    }
+
 }
